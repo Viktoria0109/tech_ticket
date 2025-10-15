f35bff3d06206d42b88eb1d15d9b7c352db2d537
Viktoria0109 <166151713+Viktoria0109@users.noreply.github.com>
2025-10-15 15:27:40 +0300

    из app\app в app

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+print("DATABASE_URL =", DATABASE_URL) 
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
