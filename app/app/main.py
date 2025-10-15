5cc5da34ec12ee6dfe6e52248355365068ae054a
Viktoria0109 <166151713+Viktoria0109@users.noreply.github.com>
2025-10-15 12:48:42 +0300

    gitignore и main

--- app/app/main.py
+++ app/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from app.api.v1 import tickets
+from app.database import Base, engine
+from app.models import user, ticket, comment 
+
 
 app = FastAPI()
 
 app.include_router(tickets.router, prefix="/api/v1")
+
+Base.metadata.create_all(bind=engine)
+
 
