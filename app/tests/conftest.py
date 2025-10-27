dc58a9d8e65a188a89f3a33c933fb9f1ab67f4a9
Viktoria0109 <166151713+Viktoria0109@users.noreply.github.com>
2025-10-27 15:03:36 +0300

    добавила conftest.py

---
+++ app/tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.models import Base  
+from app.dependencies import get_db 
+from fastapi.testclient import TestClient
+from app.main import app 
+
+TEST_SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+engine = create_engine(TEST_SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
+
+@pytest.fixture()
+def client():
+    return TestClient(app)
+
