6b3e39cc4fb84fcbb3c8767af529639a704ff414
Viktoria0109 <166151713+Viktoria0109@users.noreply.github.com>
2025-10-29 15:02:29 +0300

    подключила logger

--- app/db/base.py
+++ app/db/base.py
@@ -2,22 +2,29 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
-
+from app.core.logger import logger
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 print("DATABASE_URL =", DATABASE_URL) 
 
+logger.info("Подключение к базе данных...")
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-
+logger.info("Создание SessionLocal...")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
+logger.info("Создание базовой модели...")
 Base = declarative_base()
 
-
+def init_db():
+    logger.info("Создание таблиц...")
+    try:
+        Base.metadata.create_all(bind=engine)
+        logger.info("Таблицы успешно созданы.")
+    except Exception as e:
+        logger.error(f"Ошибка при создании таблиц: {e}")
 
