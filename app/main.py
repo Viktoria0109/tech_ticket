373d9254ea27c57295a157ae04d9b03d60b3b813
sigmund666 <pochtamixey@gmail.com>
2025-10-31 01:00:58 +0300

    структурировал main
    пока все

--- app/main.py
+++ app/main.py
@@ -1,56 +1,41 @@
 
-from fastapi import FastAPI, Depends, Request
-from . import models 
-from app.api.v1 import tickets
-from app.api.v1 import auth
+from fastapi import FastAPI, Depends, Request,HTTPException
+from pathlib import Path
+from sqlalchemy.orm import Session
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+from . import models,schemas
+from app.api.v1 import tickets,auth
 from app.db.session import Base, engine, get_db
 from app.models import user, ticket
 from app.models.comment import Comment
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from pathlib import Path
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from app import models, schemas
+from app.api.v1.users import create_admin_if_not_exists
 
 app = FastAPI()
 
 
 Base.metadata.create_all(bind=engine)
+
+
+BASE_DIR = Path(__file__).resolve().parent
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+
+app.include_router(tickets.router, prefix="/api/v1")
+app.include_router(auth.router, prefix="/api/v1")
 
 @app.get("/")
 def read_root(db = Depends(get_db)):
     return {"ok": True}
 
 
-app.include_router(tickets.router, prefix="/api/v1")
-app.include_router(auth.router, prefix="/api/v1")
-
-
-Base.metadata.create_all(bind=engine)
-app.mount("/static", StaticFiles(directory="frontent/static"), name="static")
-templates = Jinja2Templates(directory="frontent/templates")
-
-app = FastAPI()
-
-
-BASE_DIR = Path(__file__).resolve().parent
-
-
-app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
-
-
-templates = Jinja2Templates(directory=BASE_DIR / "templates")
-
-
 @app.get("/")
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
                                       
-
-from app.api.v1.users import create_admin_if_not_exists
-
-
 @app.post("/tickets", response_model=schemas.Ticket)
 def create_ticket(ticket: schemas.TicketCreate, db: Session = Depends(get_db)):
     db_ticket = models.Ticket(**ticket.dict())
